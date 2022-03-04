0b2879e1b586a1ed31b2c2a7e24fb87e57b551e7
Dev Z <dev-z@example.com>
2022-03-04 11:44:00 +0530

    Revert "commit done by developer Z on 04/03/2022"
    This reverts commit 616f2f1885b885c167948b57bbae46569b59f1ef.

--- a.java
+++ a.java
@@ -1,5 +1,4 @@
 First file added in the directory.
 My name is sprint warrior
 This is a change by Developer Z
-Change done on 03/03/2022
-Change done by developer Z on 04/03/2022
+Changed done on 03/03/2022
